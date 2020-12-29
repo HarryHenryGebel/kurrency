46db03bd7f212d0b6e5c018c8b1fe5186acdb8b9
Harry Henry Gebel <harry@gebel.tech>
2020-12-29 06:17:01 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    kotlin("multiplatform") version "1.4.21"
+}
+
+group = "gebel.tech"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useTestNG()
+        }
+    }
+    js(IR) {
+        browser {
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                }
+            }
+        }
+    }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-testng"))
+            }
+        }
+        val jsMain by getting
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+        val nativeMain by getting
+        val nativeTest by getting
+    }
+}
---
+++ src/commonMain/kotlin/Kurrency.kt
@@ -0,0 +1,89 @@
+import kotlin.math.min
+import kotlin.math.pow
+
+/**
+ *
+ */
+class Kurrency(
+    number: Number,
+    val digits: Int = 2,
+    val precision: Int = 3
+) : Number() {
+    private val multiplier = 10.0.pow(precision).toLong()
+    private val outputMultiplier = 10.0.pow(digits).toLong()
+    private val subunits = (number.toDouble() * multiplier).toLong()
+
+    operator fun unaryPlus(): Kurrency {
+        return this
+    }
+
+    operator fun unaryMinus(): Kurrency {
+        return Kurrency(-subunits.toDouble() * multiplier, digits, precision)
+    }
+
+    // TODO special cases when both sides are Kurrency instances
+
+    operator fun plus(number: Number): Kurrency {
+        return Kurrency(this.toDouble() + number.toDouble(), digits, precision)
+    }
+
+    operator fun minus(number: Number): Kurrency {
+        return Kurrency(this.toDouble() - number.toDouble(), digits, precision)
+    }
+
+    operator fun times(number: Number): Kurrency {
+        return Kurrency(this.toDouble() * number.toDouble(), digits, precision)
+    }
+
+    operator fun div(number: Number): Kurrency {
+        return Kurrency(this.toDouble() / number.toDouble(), digits, precision)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is Kurrency -> {
+                val digits = min(this.digits, other.digits)
+                val precision = min(this.precision, other.precision)
+                Kurrency(this.toDouble(), digits, precision).subunits ==
+                        Kurrency(other.toDouble(), digits, precision).subunits
+            }
+            is Number -> {
+                Kurrency(this.toDouble(), digits, precision).subunits ==
+                        Kurrency(other.toDouble(), digits, precision).subunits
+            }
+            else -> false
+        }
+    }
+
+    operator fun compareTo(number: Number): Int {
+        return (this - number).toInt()
+    }
+
+    override fun toByte(): Byte {
+        return (subunits.toDouble() / outputMultiplier).toInt().toByte()
+    }
+
+    override fun toChar(): Char {
+        return (subunits.toDouble() / outputMultiplier).toChar()
+    }
+
+    override fun toDouble(): Double {
+        return (subunits.toDouble() / outputMultiplier)
+    }
+
+    override fun toFloat(): Float {
+        return (subunits.toFloat() / outputMultiplier)
+    }
+
+    override fun toInt(): Int {
+        return (subunits.toDouble() / outputMultiplier).toInt()
+    }
+
+    override fun toLong(): Long {
+        return (subunits.toDouble() / outputMultiplier).toLong()
+    }
+
+    override fun toShort(): Short {
+        return (subunits.toDouble() / outputMultiplier).toInt().toShort()
+    }
+}
